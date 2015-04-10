42d06c9508d9fe2bc11b7ce15c77fc6401228388
wei.ren app 4dept 5680 <wei.ren@cdoa08.sunmedia.com.cn>
2015-04-10 16:20:54 +0800

    using find find file instead of svn -list for better performance.

--- personal/init_custom.el
+++ personal/init_custom.el
@@ -4,4 +4,8 @@
 (normal-erase-is-backspace-mode 1)
 ;(global-linum-mode 1)
 
+(setq projectile-globally-ignored-directories (append '(".svn" ".git") projectile-globally-ignored-directories))
+(setq projectile-globally-ignored-files (append '("*.svn-base" "*.o" "*.d" "*.pyc" "*.pyo") projectile-globally-ignored-files))
+(setq projectile-svn-command "find . -type f -not -iwholename '*.svn/*' -print0")
+
 (global-set-key (kbd "C-x c") 'save-buffers-kill-emacs)
